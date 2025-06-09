38c1d7818fd1f7a7fb1b7705af4205beb5ffba6b
scooter7 <scooter.vineburgh@gmail.com>
2025-06-08 21:21:11 -0500

    pdf-parse import with new lib/pdfProcessor11

--- app/api/source-materials/[materialId]/process/route.ts
+++ app/api/source-materials/[materialId]/process/route.ts
@@ -1,14 +1,20 @@
 // app/api/source-materials/[materialId]/process/route.ts
 
 // Polyfill for DOMMatrix in Node.js (so libraries that rely on it can run server-side)
+// We cast to `any` here to avoid having to implement all of the static methods.
 if (typeof globalThis.DOMMatrix === 'undefined') {
-  globalThis.DOMMatrix = class {
+  // @ts-ignore: Assigning a minimal polyfill to DOMMatrix
+  (globalThis as any).DOMMatrix = class {
     a: number; b: number; c: number; d: number; e: number; f: number;
 
     constructor(init?: string | number[]) {
       this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
       if (typeof init === 'string') {
-        const vals = init.replace('matrix(', '').replace(')', '').split(',').map(Number);
+        const vals = init
+          .replace('matrix(', '')
+          .replace(')', '')
+          .split(',')
+          .map(Number);
         [this.a, this.b, this.c, this.d, this.e, this.f] = vals;
       } else if (Array.isArray(init) && init.length === 6) {
         [this.a, this.b, this.c, this.d, this.e, this.f] = init;
@@ -18,6 +24,11 @@ if (typeof globalThis.DOMMatrix === 'undefined') {
     // no-op to satisfy interfaces
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     translate(tx: number, ty: number) { /* noop */ }
+
+    // Add stub static methods so TS consumers won’t break at runtime if they’re used:
+    static fromFloat32Array(array: Float32Array) { return new (globalThis as any).DOMMatrix() }
+    static fromFloat64Array(array: Float64Array) { return new (globalThis as any).DOMMatrix() }
+    static fromMatrix(other?: DOMMatrixInit) { return new (globalThis as any).DOMMatrix() }
   };
 }
 
